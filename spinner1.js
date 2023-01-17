89e6ea3f4be00a179e97fe65deec6ec150df4c02
SinaSS77 <s.molaverdikhani>
2023-01-17 11:44:23 -0800

    spinner! in a line

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   \n',);
+  console.log("sina");
+}, 700);
+
+// ... fill in the rest yourself ...
